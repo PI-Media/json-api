676cb73433baa4c9afae0c507ea8f0160465a6f7
Ali Qureshi <parorrey@yahoo.com>
2022-05-28 06:57:16 +0500

    Update json-api.php
    WordPress version 6.0

--- json-api.php
+++ json-api.php
@@ -3,7 +3,7 @@
 Plugin Name: JSON API
 Plugin URI: http://wordpress.org/plugins/json-api/
 Description: A RESTful API for WordPress
-Version: 2.2.0
+Version: 2.3.0
 Author: Dan Phiffer, Ali Qureshi
 Author URI: http://phiffer.org/, http://parorrey.com/
 */
